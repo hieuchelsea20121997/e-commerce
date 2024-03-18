7f7f5279449bc19904d55f374a6cb9f6e8fd5e83
Nguyen Tan Dat <ntd2003999@gmail.com>
2024-03-18 21:29:58 +0700

    Handle error

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 require("dotenv").config();
+const dbconnect = require("./config/dbconnect");
+const initRoutes = require("./routes");
 
 const app = express();
 const port = process.env.PORT || 8888;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("", (req, res) => {
-  res.send(" World!");
-});
+dbconnect();
+initRoutes(app);
 
 app.listen(port, () => {
   console.log(`Server is running on localhost:${port}`);
